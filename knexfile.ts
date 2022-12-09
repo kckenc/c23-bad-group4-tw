1b6a615a749ecb597b078ab5218d108543465fd0
Ken Chan <kenchan6694@gmail.com>
2022-12-09 19:04:32 +0800

    first commit

---
+++ knexfile.ts
@@ -0,0 +1,50 @@
+import type { Knex } from "knex";
+import dotenv from 'dotenv';
+
+// Update with your config settings.
+dotenv.config();
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS
+    }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+
+};
+
+module.exports = config;
