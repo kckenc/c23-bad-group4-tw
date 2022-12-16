9ce6fe2d088458c4b718e24a29cba22c2264e00d
Ken Chan <kenchan6694@gmail.com>
2022-12-16 11:00:42 +0800

    ken

--- migrations/20221209103102_create-tables.ts
+++ migrations/20221209103102_create-tables.ts
@@ -3,6 +3,7 @@ import { Knex } from "knex";
 const userTableName = "users";
 const cameraTableName = "cameras";
 const elderlyTableName = "elderly";
+const roomTableName = "room";
 const DetectionTableName = "leaving_detection"
 
 export async function up(knex: Knex): Promise<void> {
@@ -15,28 +16,33 @@ export async function up(knex: Knex): Promise<void> {
   
   await knex.schema.createTable(cameraTableName, (table)=> {
     table.increments();
-    table.string("ip").unique();
-    table.integer("connected_to").unsigned().notNullable;
-    table.foreign("connected_to").references(`${userTableName}.id`)
+    table.string("mac_address").unique();
     table.timestamps(false, true);
-
   })  
   
+  await knex.schema.createTable(roomTableName, (table)=>{
+    table.increments();
+    table.string("room_tag");
+    table.string("user_id").unsigned().notNullable;
+    table.foreign("user_id").references(`${userTableName}.id`);
+  })
+
   await knex.schema.createTable(elderlyTableName, (table)=> {
     table.increments();
     table.string("name");
-    table.string("room").notNullable;
+    table.string("description");
     table.integer("camera_id").unsigned().notNullable;
-    table.foreign("camera_id").references(`${cameraTableName}.id`)
+    table.foreign("camera_id").references(`${cameraTableName}.id`);
+    table.integer("room_id").unsigned().notNullable;
+    table.foreign("room_id").references(`${roomTableName}.id`);
     table.timestamps(false, true);
   })  
-  
+
   await knex.schema.createTable(DetectionTableName, (table)=> {
     table.increments();
-    table.string("image");
-    table.integer("no_of_time");
+    table.boolean("is_solved").defaultTo(true);
     table.integer("elderly_id").unsigned().notNullable;
-    table.foreign("elderly_id").references(`${elderlyTableName}.id`)
+    table.foreign("elderly_id").references(`${elderlyTableName}.id`);
     table.timestamps(false, true);
   })
   // await knex.schema.alterTable(DetectionTableName, (table) =>{
@@ -46,10 +52,11 @@ export async function up(knex: Knex): Promise<void> {
 
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists(userTableName);
-  await knex.schema.dropTableIfExists(cameraTableName);
-  await knex.schema.dropTableIfExists(elderlyTableName);
   await knex.schema.dropTableIfExists(DetectionTableName);
+  await knex.schema.dropTableIfExists(elderlyTableName);
+  await knex.schema.dropTableIfExists(roomTableName);
+  await knex.schema.dropTableIfExists(cameraTableName);
+  await knex.schema.dropTableIfExists(userTableName);
   // await knex.schema.alterTable(DetectionTableName, (table) => {
   //   table.dropColumn("image")
   // });
--- seeds/dummy.ts
+++ seeds/dummy.ts
@@ -10,22 +10,22 @@ export async function seed(knex: Knex): Promise<void> {
   ];
 
   const elderly = [
-    {name: "mary", room: "A"},
-    {name: "betty", room: "B"},
-    {name: "john", room: "A"},
-    {name: "tracy", room: "C"},
-    {name: "scarlett", room: "B"},
+    {name: "mary", description: ""},
+    {name: "betty",description: ""},
+    {name: "john",description: ""},
+    {name: "tracy",description: ""},
+    {name: "scarlett",description: ""},
   ];
 
-  const cameras = [
-    {ip:"192.168.1.104"},
-    {ip:"192.168.1.20"},
-    {ip:"192.168.1.102"},
-    {ip:"192.168.1.109"},
-    {ip:"192.168.1.45"},
+  const room = [
+    {room_tag: "A"},
+    {room_tag: "B"},
+    {room_tag: "C"},
+
   ]
+
   // Inserts seed entries
   await knex("users").insert(users);
   await knex("elderly").insert(elderly);
-  await knex("cameras").insert(cameras);
+  await knex("cameras").insert(room);
 }
