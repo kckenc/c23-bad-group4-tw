9605865c3be0fcfc7845ee0037bf5500129e0122
Ken Chan <kenchan6694@gmail.com>
2022-12-20 11:05:19 +0800

    camera html js

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,16 +1,76 @@
-import{loadHeader, loadNavBar, loadFooter} from "../js/layout.js"
+
 
 window.onload = () => {
   loadHeader();
   loadNavBar();
   loadFooter();
   closeAlertButton();
+  initIframe();
 }
 
 function closeAlertButton() {
-	document.querySelector('#dylan').addEventListener('click', async () => {
-        console.log("lmsl")
-		await fetch('/bye')
+	// document.querySelector('#dylan').addEventListener('click', async () => {
+  //       console.log("lmsl")
+	// 	await fetch('/bye')
 		
-	})
+	// })
+}
+function setAttributes(el, attrs) {
+  for(let key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
+function initIframe() {
+  const camMainDiv = document.querySelector('.cam-container');
+  const eleDiv = document.createElement('div');
+  const roomDiv = document.createElement('div');
+  const iFrame = document.createElement('iframe');
+  setAttributes(iFrame, {"class":"revert","src": "http://192.168.160.78:5431", "width": "350", "height": "200"});
+  const closeAlert = document.createElement('button');
+  setAttributes(closeAlert, {"class":"close-alert", "data-user": "dylan"});
+  closeAlert.innerText = "close alert";
+  const elderlyNameDiv = document.createElement('div');
+  setAttributes(elderlyNameDiv, {"class":"elder-name"});
+  elderlyNameDiv.innerText = "Dylan";
+
+  camMainDiv.appendChild(eleDiv);
+  eleDiv.appendChild(roomDiv);
+  eleDiv.appendChild(iFrame);
+  eleDiv.appendChild(closeAlert);
+  eleDiv.appendChild(elderlyNameDiv);
+
+  const eleDiv2 = document.createElement('div');
+  const roomDiv2 = document.createElement('div');
+  const iFrame2 = document.createElement('iframe');
+  setAttributes(iFrame2, {"class":"revert","src": "http://192.168.160.149:5430", "width": "350", "height": "200"});
+  const closeAlert2 = document.createElement('button');
+  setAttributes(closeAlert2, {"class":"close-alert", "data-user": "ken"});
+  closeAlert2.innerText = "close alert";
+  const elderlyNameDiv2 = document.createElement('div');
+  setAttributes(elderlyNameDiv2, {"class":"elder-name"});
+  elderlyNameDiv2.innerText = "Ken";
+
+  camMainDiv.appendChild(eleDiv2);
+  eleDiv2.appendChild(roomDiv2);
+  eleDiv2.appendChild(iFrame2);
+  eleDiv2.appendChild(closeAlert2);
+  eleDiv2.appendChild(elderlyNameDiv2);
+
+  const eleDiv3 = document.createElement('div');
+  const roomDiv3 = document.createElement('div');
+  const iFrame3 = document.createElement('iframe');
+  setAttributes(iFrame3, {"class":"revert","src": "http://192.168.160.25:5432", "width": "350", "height": "200"});
+  const closeAlert3 = document.createElement('button');
+  setAttributes(closeAlert3, {"class":"close-alert", "data-user": "kyle"});
+  closeAlert2.innerText = "close alert";
+  const elderlyNameDiv3 = document.createElement('div');
+  setAttributes(elderlyNameDiv3, {"class":"elder-name"});
+  elderlyNameDiv3.innerText = "Kyle";
+
+  camMainDiv.appendChild(eleDiv3);
+  eleDiv3.appendChild(roomDiv3);
+  eleDiv3.appendChild(iFrame3);
+  eleDiv3.appendChild(closeAlert3);
+  eleDiv3.appendChild(elderlyNameDiv3);
 }
--- public/js/layout.js
+++ public/js/layout.js
@@ -1,6 +1,6 @@
 // import {closeAlertButton} from "../js/camera"
 // window.onload = () => {
-  
+
 //   // logIn();
 //   loadHeader();
 //   loadNavBar();
@@ -10,15 +10,15 @@
 //   // register()
 // }
 
-export function loadHeader() {
+function loadHeader() {
   const header = document.querySelector(".header");
   const leftSide = document.createElement("div");
-  leftSide.classList.add("left")
+  leftSide.classList.add("left");
   const rightSide = document.createElement("div");
-  rightSide.classList.add("right")
+  rightSide.classList.add("right");
   const logoDiv = document.createElement("img");
   logoDiv.classList.add("logo");
-  logoDiv.setAttribute('src',`./asset/logo.png`);
+  logoDiv.setAttribute("src", `./asset/logo.png`);
   const titleDiv = document.createElement("h4");
   titleDiv.classList.add("title", "text");
   titleDiv.innerText = "Elder Ring";
@@ -28,11 +28,11 @@ export function loadHeader() {
   const logoutDiv = document.createElement("div");
   logoutDiv.classList.add("logoutDiv");
   const logoutLogo = document.createElement("img");
-  logoutLogo.classList.add("logoutImg")
-  logoutLogo.setAttribute('src',`./asset/logout.png`);
+  logoutLogo.classList.add("logoutImg");
+  logoutLogo.setAttribute("src", `./asset/logout.png`);
   const logoutWord = document.createElement("h4");
   logoutWord.classList.add("logoutWord", "text");
-  logoutWord.innerText = "Logout"
+  logoutWord.innerText = "Logout";
 
   header.appendChild(leftSide);
   leftSide.appendChild(logoDiv);
@@ -47,38 +47,39 @@ function loadNavBar() {
   console.log("im navbar");
   const nav = document.querySelector(".sideNav");
   const container = document.createElement("div");
-  container.classList.add("navContainer");  ``
+  container.classList.add("navContainer");
+  ``;
   const homeDiv = document.createElement("div");
-  homeDiv.classList.add("homeDiv","navShare");
+  homeDiv.classList.add("homeDiv", "navShare");
   const homeLogo = document.createElement("img");
   homeLogo.classList.add("navLogoSize");
-  homeLogo.setAttribute("src",`./asset/home.png`);
+  homeLogo.setAttribute("src", `./asset/home.png`);
   const homeText = document.createElement("div");
-  homeText.classList.add("navText")
+  homeText.classList.add("navText");
   homeText.innerText = "Home";
   const camDiv = document.createElement("div");
-  camDiv.classList.add("camDiv","navShare");
+  camDiv.classList.add("camDiv", "navShare");
   const camLogo = document.createElement("img");
   camLogo.classList.add("navLogoSize");
-  camLogo.setAttribute("src",`./asset/cam.png`);
+  camLogo.setAttribute("src", `./asset/cam.png`);
   const camText = document.createElement("div");
-  camText.classList.add("navText")
+  camText.classList.add("navText");
   camText.innerText = "Cameras";
   const settingDiv = document.createElement("div");
-  settingDiv.classList.add("settingDiv","navShare");
+  settingDiv.classList.add("settingDiv", "navShare");
   const settingLogo = document.createElement("img");
-  settingLogo.setAttribute("src",`./asset/setting.png`);
+  settingLogo.setAttribute("src", `./asset/setting.png`);
   settingLogo.classList.add("navLogoSize");
   const settingText = document.createElement("div");
-  settingText.classList.add("navText")
+  settingText.classList.add("navText");
   settingText.innerText = "Setting";
   const aboutDiv = document.createElement("div");
   aboutDiv.classList.add("aboutDiv", "navShare");
   const aboutLogo = document.createElement("img");
-  aboutLogo.setAttribute("src",`./asset/about.png`);
+  aboutLogo.setAttribute("src", `./asset/about.png`);
   aboutLogo.classList.add("navLogoSize");
   const aboutText = document.createElement("div");
-  aboutText.classList.add("navText")
+  aboutText.classList.add("navText");
   aboutText.innerText = "About";
 
   nav.appendChild(container);
@@ -104,12 +105,12 @@ function loadNavBar() {
 //   footerText.innerText = "ElderRing/ All rights reserved."
 //   footer.appendChild(footerText)
 // }
-export function loadFooter() {
+function loadFooter() {
   const footer = document.querySelector(".footer");
   const footerText = document.createElement("div");
   footerText.classList.add("copyright");
-  footerText.innerText = "ElderRing/ All rights reserved."
-  footer.appendChild(footerText)
+  footerText.innerText = "ElderRing/ All rights reserved.";
+  footer.appendChild(footerText);
 }
 
 // function initLoginForm() {
@@ -137,7 +138,6 @@ export function loadFooter() {
 // 		}
 // 	  });
 //   }
-  
 
 // function register() {
 // const registerbutton = document.querySelector('#register')
